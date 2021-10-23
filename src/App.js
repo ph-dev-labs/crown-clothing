2f6613af0adc93d015651b495d26d76a10aa6d8e
ph-dev-labs <nwosuchimezie95@gmail.com>
2021-10-23 22:48:52 +0200

    update our menu-items to navigate HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router';
 import './App.css';
 
 import Homepage from './pages/homepage/home.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </switch>
     </div>
   );
 }
